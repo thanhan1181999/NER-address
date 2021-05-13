3701c16de627972c599a8a04f991cbca3fe30b4d
Nguyễn Thành An <thanhannguyen211@gmail.com>
2021-05-13 18:50:51 +0700

    fix process_data_caugiay

--- 1. data csv/process_caugiay_data.py
+++ 1. data csv/process_caugiay_data.py
@@ -18,7 +18,7 @@
 
 # ================START ĐẦU TIÊN=====================
 import pandas as pd 
-names = ["address_parts.number","address_parts.street","parent.locality","parent.county","parent.region","parent.country","name.default"]
+names = ["address_parts.number","address_parts.street","parent.locality","parent.county","parent.region","parent.country","name.default","layer"]
 
 data = pd.read_csv('caugiay.csv', encoding='utf8',usecols=names)
 # fill "" to empty data
@@ -29,30 +29,45 @@ data['parent.county'] = data['parent.county'].fillna("")
 data['parent.region'] = data['parent.region'].fillna("")
 data['parent.country'] = data['parent.country'].fillna("")
 data['name.default'] = data['name.default'].fillna("")
+data['layer'] = data['layer'].fillna("")
 # lowercase + bỏ dấu ,
 print("begin lowercase + bỏ dấu , ...")
 for index in range(len(data)):
   data['address_parts.number'][index] = data['address_parts.number'][index].lower().replace(',',' ').replace('_',' ')
   data['address_parts.street'][index] = data['address_parts.street'][index].lower().replace(',',' ').replace('_',' ').replace('-',' ')
   data['parent.locality'][index]      = data['parent.locality'][index][2:-2].lower()
-  data['parent.county'][index]        = data['parent.county'][index][2:-2].lower()
+  data['parent.county'][index]       = data['parent.county'][index][2:-2].lower()
   data['parent.region'][index]        = data['parent.region'][index][2:-2].lower()
   data['parent.country'][index]       = data['parent.country'][index][2:-2].lower()
-  data['name.default'][index]         = data['name.default'][index].lower().replace(',',' ').replace('-',' ').replace('&',' ').replace('_',' ')
+  data['name.default'][index]         = data['name.default'][index].lower().replace(',',' ').replace('-',' ').replace('&',' ').replace('_',' ').replace('[',' ').replace(']',' ').replace('"',' ')
 print("end lowercase + bỏ dấu ,")
 print('\n')
 # ================END ĐẦU TIÊN=====================
 
+# bỏ trường locaity (xã) ở bản ghi (random có thể bỏ hoặc không)
+import random
+for index in range(len(data)):
+  x = random.randint(1, 10)
+  if x % 2 == 0 :
+    data['parent.locality'][index]=""
 
+# bỏ trường country (quốc gia) ở bản ghi (random có thể bỏ hoặc không)
+import random
+for index in range(len(data)):
+  x = random.randint(1, 10)
+  if x % 2 == 0 :
+    data['parent.country'][index]=""
 
 # ================START BƯỚC 1=====================
 print("begin get ner...")
 caugiay_ner = []
 for index in range(len(data)):
-  housenumber = data['address_parts.number'][index]
-  street = data['address_parts.street'][index]
+  # housenumber = data['address_parts.number'][index]
+  # street = data['address_parts.street'][index]
   name = data['name.default'][index]
-  if name!="" and (housenumber+" "+street)!=name:
+  layer = data['layer'][index]
+  # if name!="" and (housenumber+" "+street)!=name:
+  if layer=="venue":
     caugiay_ner.append(name)
 # export to file
 ner_file = open("../2. primary data/ner.txt","a",encoding='utf8')
@@ -158,10 +173,15 @@ print("end xử lý số nhà")
 print("\n")
 # ================END BƯỚC 3 xử lý số nhà=======================
 
-
-
+# bỏ những bản ghi có số nhà, nhưng ko có tên đường
+for index in range(len(data)):
+  if data['address_parts.street'][index]=="" and data['address_parts.number'][index]!="":
+    data['address_parts.number'][index]=""
 
 # ================START BƯỚC 4 loại bỏ trùng lặp và lưu file=====================
+print("Ban đầu có {} bản ghi".format(len(data)))
+print("đang lọc...")
+
 def map_number_and_punct(word):
     # check hem va ngach
     num_of_seperate=0
@@ -197,6 +217,8 @@ set_of_record = {"pad"} # to check sentence is exist ?
 set_of_record.clear()
 # len(concatate_data)
 for index in range(len(data)):
+  print("sen {}".format(index))
+
   number = data['address_parts.number'][index]
   street = data['address_parts.street'][index]
   locality = data['parent.locality'][index]
@@ -206,10 +228,10 @@ for index in range(len(data)):
 
   arr_number = annotator.tokenize(number.lower())
   arr_street = annotator.tokenize(street.lower())
-  arr_locality = annotator.tokenize(locality.lower())
-  arr_county = annotator.tokenize(county.lower())
-  arr_region = annotator.tokenize(region.lower())
-  arr_country = annotator.tokenize(country.lower())
+  # arr_locality = annotator.tokenize(locality.lower())
+  # arr_county = annotator.tokenize(county.lower())
+  # arr_region = annotator.tokenize(region.lower())
+  # arr_country = annotator.tokenize(country.lower())
 
   concate_token = []
   
@@ -220,16 +242,20 @@ for index in range(len(data)):
     concate_token.extend(arr_street[0])
   
   if locality!="":
-    concate_token.extend(arr_locality[0])
+    # concate_token.extend(arr_locality[0])
+    concate_token.append(locality)
   
   if county!="":
-    concate_token.extend(arr_county[0])
+    # concate_token.extend(arr_county[0])
+    concate_token.append(county)
 
   if region!="":
-    concate_token.extend(arr_region[0])
+    # concate_token.extend(arr_region[0])
+    concate_token.append(region)
   
   if country!="":
-    concate_token.extend(arr_country[0])
+    # concate_token.extend(arr_country[0])
+    concate_token.append(country)
   
   for idx in range(len(concate_token)):
     element = concate_token[idx]
@@ -248,7 +274,6 @@ for index in range(len(data)):
       'country':country
     })
 
-print("Ban đầu có {} bản ghi".format(len(data)))
 # print(len(set_of_record))
 print("Sau khi lọc, còn lại {} bản ghi".format(len(need_data['caugiay'])))
 
